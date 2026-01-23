3c7798e2ea2a98db9b71fddaf39b9d6ac4987f0a
Limo123123 <limo64794@gmail.com>
2026-01-23 21:48:26 +0100

    Changed API

--- account/script.js
+++ account/script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     let currentUser = null;
-    const API_BASE_URL = 'https://limazonapi.any64.de';
+    const API_BASE_URL = 'https://raspberrypi.tail75d81e.ts.net:8443';
 
     // --- DATENABRUF UND ANZEIGE ---
     async function fetchUserData() {
